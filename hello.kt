d9f54c861fbd251d1a6153287d9564940c1c281c
Kenny4571 <119124617+Kenny4571@users.noreply.github.com>
2023-01-31 16:19:54 -0600

    Create hello.kt

---
+++ hello.kt
@@ -0,0 +1,7 @@
+fun main() {
+    println("Hello World!")
+    println("I am learning Kotlin.")
+    println("It is awesome!")
+    println("Hello, world!!!")
+    println("My name is Asanat, and I'm learning to program.")
+}
